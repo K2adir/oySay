9db1a0c323e7164a73583cf094c9f1b6be225745
K2adir <k2adir@gmail.com>
2023-05-19 23:52:59 -0400

    btn color

--- src/components/ProfileVoteAdd/ProfileVoteAdd.jsx
+++ src/components/ProfileVoteAdd/ProfileVoteAdd.jsx
@@ -2,25 +2,46 @@ import { VoteContext } from "../../context/VoteContext";
 import { useContext } from "react";
 import "./ProfileVoteAdd.scss";
 import { HiOutlinePlus } from "react-icons/hi";
+import { HiMinus } from "react-icons/hi";
+
 const ProfileVoteAdd = () => {
   const { recepVotes, setRecepVotes, kemalVotes, setKemalVotes } =
     useContext(VoteContext);
 
   return (
-    <div className="dashboard__container">
-      <button
-        className="profile__add_btn"
-        onClick={() => setRecepVotes(recepVotes + 1)}
-      >
-        <HiOutlinePlus style={{ color: "white" }} />
-      </button>
-      <button
-        className="profile__add_btn"
-        onClick={() => setKemalVotes(kemalVotes + 1)}
-      >
-        <HiOutlinePlus style={{ color: "white" }} />
-      </button>
-    </div>
+    <>
+      <div className="dashboard__container">
+        <button
+          className="profile__add_btn"
+          onClick={() => setRecepVotes(recepVotes + 1)}
+        >
+          <HiOutlinePlus style={{ color: "white" }} />
+        </button>
+        <button
+          className="profile__add_btn"
+          onClick={() => setKemalVotes(kemalVotes + 1)}
+        >
+          <HiOutlinePlus style={{ color: "white" }} />
+        </button>
+      </div>
+      {/* // */}
+      {/* // */}
+      {/* // */}
+      <div className="dashboard__container">
+        <button
+          className="profile__minus_btn"
+          onClick={() => setRecepVotes(recepVotes + 1)}
+        >
+          <HiMinus style={{ color: "white" }} />
+        </button>
+        <button
+          className="profile__minus_btn"
+          onClick={() => setKemalVotes(kemalVotes + 1)}
+        >
+          <HiMinus style={{ color: "white" }} />
+        </button>
+      </div>
+    </>
   );
 };
 
