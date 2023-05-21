a2dfae7b9de9c2b4b9b2e7209c4a90438dda8bca
K2adir <k2adir@gmail.com>
2023-05-20 20:37:14 -0400

    results

--- src/components/Scores/Scores.jsx
+++ src/components/Scores/Scores.jsx
@@ -1,10 +1,16 @@
 import "./Scores.scss";
-
+import { RxCross1 } from "react-icons/rx";
+import { RxEnvelopeClosed } from "react-icons/rx";
+import { RxEnvelopeOpen } from "react-icons/rx";
+import { MdPeopleOutline } from "react-icons/md";
 const Scores = () => {
   return (
     <div className="dashboard__container">
       <div className="scores__container">
         <div className="scores__row">
+          <div className="scores__icon">
+            <RxCross1 />
+          </div>
           <div className="scores__text">
             <h4>Geçersiz Oy</h4>
           </div>
@@ -15,6 +21,9 @@ const Scores = () => {
         {/* /// */}
         {/* /// */}
         <div className="scores__row">
+          <div className="scores__icon">
+            <RxEnvelopeOpen />
+          </div>
           <div className="scores__text">
             <h4>Açılan Zarf</h4>
           </div>
@@ -25,6 +34,9 @@ const Scores = () => {
         {/*  */}
         {/*  */}
         <div className="scores__row">
+          <div className="scores__icon">
+            <RxEnvelopeClosed />
+          </div>
           <div className="scores__text">
             <h4>Sandıktan Çıkan Zarf</h4>
           </div>
@@ -35,6 +47,9 @@ const Scores = () => {
         {/*  */}
         {/*  */}
         <div className="scores__row">
+          <div className="scores__icon">
+            <MdPeopleOutline />
+          </div>
           <div className="scores__text">
             <h4>Seçmen Sayısı</h4>
           </div>
