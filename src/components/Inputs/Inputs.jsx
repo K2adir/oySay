9f23319119c1e7d16844bd63c1fcd63dc53f1fdc
K2adir <k2adir@gmail.com>
2023-05-26 16:54:47 -0400

    .

--- src/components/Inputs/Inputs.jsx
+++ src/components/Inputs/Inputs.jsx
@@ -4,8 +4,9 @@ import React from "react";
 const Inputs = () => {
   return (
     <div className="dashboard__container input__container">
-      <input placeholder="Okul Adı:" />
+      <input className="input__input" placeholder="Okul Adı:" />
       <input
+        className="input__input"
         // type="number"
         inputMode="numeric"
         pattern="[0-9]*"
