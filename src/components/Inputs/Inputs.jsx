a6544ea2cb628cf2c0a73f82b1fe28838a0bac04
K2adir <k2adir@gmail.com>
2023-05-26 16:44:19 -0400

    input numeric

--- src/components/Inputs/Inputs.jsx
+++ src/components/Inputs/Inputs.jsx
@@ -5,7 +5,12 @@ const Inputs = () => {
   return (
     <div className="dashboard__container input__container">
       <input placeholder="Okul Adı:" />
-      <input placeholder="Sandık No:" />
+      <input
+        type="number"
+        inputMode="numeric"
+        pattern="[0-9]*"
+        placeholder="Sandık No:"
+      />
     </div>
   );
 };
