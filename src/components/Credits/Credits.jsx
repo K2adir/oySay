43d1663d862bed0269801637f88fde594479a3d4
K2adir <k2adir@gmail.com>
2023-05-25 23:35:40 -0400

    credit - aligment left

--- src/components/Credits/Credits.jsx
+++ src/components/Credits/Credits.jsx
@@ -2,26 +2,43 @@ import { useState } from "react";
 import "./Credits.scss";
 import { AiOutlineEye } from "react-icons/ai";
 import { AiOutlineEyeInvisible } from "react-icons/ai";
+import { FiGithub } from "react-icons/fi";
+import { FiInstagram } from "react-icons/fi";
+import { CiLinkedin } from "react-icons/ci";
 
 const Credits = () => {
   const [showCredits, setShowCredits] = useState(false);
+
   return (
     <div className="credits__container">
       <button
-        className={showCredits === true ? "credits_btn_up" : "credits_btn_down"}
-        onClick={(e) => {
-          setShowCredits(!showCredits);
-        }}
+        className={showCredits ? "credits_btn_up" : "credits_btn_down"}
+        onClick={() => setShowCredits(!showCredits)}
       >
-        {showCredits === false ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+        {showCredits ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
       </button>
       <div
-        className={
-          showCredits === true ? "credits__content" : "credits__content_hide"
-        }
+        className={showCredits ? "credits__content" : "credits__content_hide"}
       >
-        <p>Kadir Kirisci: Code</p>
-        <p>Deniz Turan: UI/UX</p>
+        <p>
+          <span>
+            Kadir Kirisci:
+            <span className="credit__icon">
+              <FiInstagram />
+              <CiLinkedin />
+              <FiGithub />
+            </span>
+          </span>
+        </p>
+        <p>
+          <span>Deniz Turan:</span>
+        </p>
+        <p>
+          <span className="credit__icon">
+            <FiInstagram />
+            <CiLinkedin />
+          </span>
+        </p>
       </div>
     </div>
   );
---
+++ src/components/Inputs/Inputs.jsx
@@ -0,0 +1,13 @@
+import "./Inputs.scss";
+import React from "react";
+
+const Inputs = () => {
+  return (
+    <div className="dashboard__container input__container">
+      <input placeholder="Okul Adı:" />
+      <input placeholder="Sandık No:" />
+    </div>
+  );
+};
+
+export default Inputs;
