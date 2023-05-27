b86d6ff9819c233352ae5e203d3720029e341178
Kadir <k2@MacBook-Pro.local>
2023-05-27 18:09:56 -0400

    basic input scss edits

--- src/components/Inputs/Inputs.jsx
+++ src/components/Inputs/Inputs.jsx
@@ -1,53 +1,47 @@
 import { useContext } from "react";
 import { VoteContext } from "../../context/VoteContext";
 import "./Inputs.scss";
+import { MdHowToVote } from "react-icons/md";
+import { TbSchool } from "react-icons/tb";
 
 const Inputs = () => {
   const { sandikNo, setSandikNo, okulAdi, setOkulAdi } =
     useContext(VoteContext);
 
-  const handleSandikNoChange = (event) => {
-    const inputValue = event.target.value;
-    const prefix = "Sandık# ";
-
-    if (inputValue === "") {
-      setSandikNo("");
-    } else if (inputValue === prefix || inputValue === prefix.slice(0, -1)) {
-      setSandikNo(prefix);
-    } else if (inputValue.startsWith(prefix)) {
-      setSandikNo(inputValue);
-    } else {
-      setSandikNo(prefix + inputValue);
-    }
-  };
-
-  const handleSandikNoBlur = () => {
-    if (sandikNo === "Sandık# ") {
-      setSandikNo("");
-    }
-  };
-
   const handleOkulAdiChange = (event) => {
     setOkulAdi(event.target.value);
   };
 
+  const handleSandikNoChange = (event) => {
+    setSandikNo(event.target.value);
+  };
+
   return (
-    <div className="dashboard__container input__container">
-      <input
-        className="input__input"
-        placeholder="Okul Adı:"
-        value={okulAdi}
-        onChange={handleOkulAdiChange}
-      />
-      <input
-        className="input__input"
-        inputMode="numeric"
-        pattern="[0-9]*"
-        placeholder="Sandık No:"
-        value={sandikNo}
-        onChange={handleSandikNoChange}
-        onBlur={handleSandikNoBlur}
-      />
+    <div className="input__container">
+      <div className="input__okul">
+        <label htmlFor="" className="input__label">
+          <TbSchool className="input__icon" />
+        </label>
+        <input
+          className="input__input "
+          placeholder="Okul Adı"
+          value={okulAdi}
+          onChange={handleOkulAdiChange}
+        />
+      </div>
+      <div className="input__sandik">
+        <label htmlFor="" className="input__label input__label-gap">
+          <MdHowToVote className="input__icon" />
+        </label>
+        <input
+          className="input__input"
+          inputMode="numeric"
+          pattern="[0-9]*"
+          placeholder="Sandık No"
+          value={sandikNo}
+          onChange={handleSandikNoChange}
+        />
+      </div>
     </div>
   );
 };
