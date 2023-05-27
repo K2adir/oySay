b6f60788c4c20c5b2f193f07fe7529267740efb1
K2adir <k2adir@gmail.com>
2023-05-26 21:10:49 -0400

    sandik input - ux improvement
    whenever user types in something,
    sandik # appears,
    if user deletes #, everything gets deleted

--- src/components/Inputs/Inputs.jsx
+++ src/components/Inputs/Inputs.jsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
 import "./Inputs.scss";
 
 const Inputs = () => {
+  const [sandikNo, setSandikNo] = useState("");
+
+  const handleInputChange = (event) => {
+    const inputValue = event.target.value;
+    if (inputValue === "Sandik #") {
+      setSandikNo("");
+    } else {
+      setSandikNo(
+        inputValue.startsWith("Sandik #")
+          ? inputValue
+          : `Sandik # ${inputValue}`
+      );
+    }
+  };
+
+  const handleInputBlur = () => {
+    if (sandikNo === "Sandik #") {
+      setSandikNo("");
+    }
+  };
+
   return (
     <div className="dashboard__container input__container">
       <input className="input__input" placeholder="Okul Adı:" />
       <input
         className="input__input"
-        // type="number"
         inputMode="numeric"
         pattern="[0-9]*"
         placeholder="Sandık No:"
+        value={sandikNo}
+        onChange={handleInputChange}
+        onBlur={handleInputBlur}
       />
     </div>
   );
